f85b62e04245d444af7b50c081e7d513ea363ddf
SebastianMerino <109495963+SebastianMerino@users.noreply.github.com>
2023-12-16 14:17:55 -0500

    added logic to train from epoch X

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     # training hyperparameters
     batch_size = 16  # 4 for testing, 16 for training
     n_epoch = 50
-    l_rate = 1e-5
+    l_rate = 1e-6  # changing from 1e-5 to 1e-6
 
     # Loading Data
     input_folder = r'C:\Users\smerino.C084288\Documents\simulatedCystDataset\downs800_0.0Att\input_id'
